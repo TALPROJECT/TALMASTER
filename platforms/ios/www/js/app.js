94684321611b03e7a5ed0f49650c2602e67cddb0
Alex <alexandre.attia@wanadoo.fr>
2015-07-30 02:56:47 +0200

    Platform iOS

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,102 +1,147 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngAnimate'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  // angular.module is a global place for creating, registering and retrieving Angular modules
+  // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+  // the 2nd parameter is an array of 'requires'
+  // 'starter.services' is found in services.js
+  // 'starter.controllers' is found in controllers.js
+  angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.directives','ngAnimate'])
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+
+
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+
+    $ionicPlatform.on("resume",function(){
+          User.checkSession().then(function(hasSession) {
+          if (!hasSession){ $state.go('connexion');}
+          else{$state.go('tab.dash');}
+        });
+    })
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleLightContent();
+      }
+    });
+  })
+
+  .config(function($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+
+    // Each tab has its own nav history stack:
+
+    .state('tab.dash', {
+      url: '/dash',
+      views: {
+        'tab-dash': {
+          templateUrl: 'templates/tab-dash.html',
+          controller: 'DashCtrl'
+        }
+      }
+    })
+
+    .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/tab-chats.html',
+            controller: 'ChatsCtrl'
+          }
+        }
+      })
+    .state('tab.newfriend', {
+        url: '/newfriend',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/newfriend.html',
+            controller: 'NewFriendCtrl'
+          }
+        }
+      })
+     .state('tab.allfriend', {
+        url: '/allfriend',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/allfriend.html',
+            controller: 'AllFriendCtrl'
+          }
+        }
+      })
+      .state('tab.chat-detail', {
+        url: '/chats/:chatId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/chat-detail.html',
+            controller: 'ChatDetailCtrl'
+          }
+        }
+      })
+  .state('tab.list-detail', {
+        url: '/lists/:listId',
+        views: {
+          'tab-chats': {
+            templateUrl: 'templates/list-detail.html',
+            controller: 'ListDetailCtrl'
+          }
+        }
+      })
+  .state('tab.account', {
+      url: '/account',
+      views: {
+        'tab-favorites': {
+          templateUrl: 'templates/tab-account.html',
+          controller: 'AccountCtrl'
+        }
+      }
+    })
+
+  .state('connexion', {
+      url: '/',
+      templateUrl: 'templates/connexion.html',
+      controller: 'ConnexionCtrl'
+    })
+
+  .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    })
+
+  .state('register', {
+      url: '/register',
+      templateUrl: 'templates/register.html',
+      controller: 'RegisterCtrl'
+    })
+    .state('tab.favorites', {
+      url: '/favorites',
+      views: {
+        'tab-favorites': {
+          templateUrl: 'templates/tab-favorites.html',
+          controller: 'FavoritesCtrl'
+        }
+      }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
+
   });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-.state('tab.list-detail', {
-      url: '/lists/:listId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/list-detail.html',
-          controller: 'ListDetailCtrl'
-        }
-      }
-    })
-.state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-favorites': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  })
-  .state('tab.favorites', {
-    url: '/favorites',
-    views: {
-      'tab-favorites': {
-        templateUrl: 'templates/tab-favorites.html',
-        controller: 'FavoritesCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,11 +1,23 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $state) {
+.controller('DashCtrl', function($scope, $state, $localStorage) {
    
+ $localStorage.setObject('userFavoriteArray', []);
+
     $scope.inArray= function(string, array){
       var result = false;
       for(i=0; i<array.length; i++){
         if(array[i] == string){
+            result = true;
+        }
+      }
+    return result;
+    }
+
+    $scope.inList = function(string, list){
+      var result = false;
+      for(i=0; i<list.length; i++){
+        if(list[i].name == string){
             result = true;
         }
       }
@@ -19,22 +31,64 @@ angular.module('starter.controllers', [])
     },{ 
     name:  "plus",
     artist:"",
-    face :"img/plus.png"
-  }];
+    face :"img/plus.png",
+    }];
+
+    $scope.swapIndexes = function (array, index1, index2){
+      var tempArray = array;
+      array[index1]=tempArray[index2];
+      array[index2]=tempArray[index1];
+    };
 
     $scope.addNewSong=function(newSong){
-      if( !($scope.inArray(newSong, $scope.currentList) || $scope.currentList.length>11)){
-        $scope.currentList.push(newSong);
-        $scope.currentList.newSong.face="http://www.dschool.fr/wp-content/uploads/2015/06/EliottJabes.jpg";
+      if( !($scope.inList(newSong, $scope.currentList) || $scope.currentList.length>11)){
+        $scope.currentList.splice($scope.currentList.length-1, 0,{
+          name : newSong,
+          artist : 'Artist of' + newSong,
+          face :"https://static9.viadeo-static.com/servlet/photo?memberId=0021nmwfnm67e5l1&height=185&width=140&ts=1416406120000"});
       }
     };
 
-$scope.account = function(){
-    $state.go('tab.account');
-  };
+    $scope.account = function(){
+     $state.go('tab.account');
+    };
 })
 
-.controller('ChatsCtrl', function($scope, Chats, $state) {
+.controller('ConnexionCtrl', function($scope, $state,$timeout) {
+  $scope.openLogin=function(){
+           $timeout(function() {
+      // $timeout to allow animation to complete
+      $state.go('login');
+    }, 750);
+
+  }
+  $scope.openRegister=function(){
+    $state.go('register');
+  }
+})
+.controller('LoginCtrl', function($scope,User, $stateParams, $localStorage,$location) {
+  $scope.connect=function(user){
+    console.log(user);
+ 
+    $localStorage.setObject('user',user);               
+                 // $ionicLoading.hide();
+                 console.log("here");
+                  $location.path('/tab/dash');
+
+}
+})
+.controller('RegisterCtrl', function($scope, User, $stateParams, $localStorage,$location) {
+  $scope.regularConnect=function(user){
+    console.log(user);
+ 
+             $localStorage.setObject('user',user);               
+                 // $ionicLoading.hide();
+                 console.log("here");
+                  $location.path('/tab/dash');
+
+}
+})
+.controller('ChatsCtrl', function($scope, Chats, $state, $ionicModal) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -42,16 +96,90 @@ $scope.account = function(){
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+ $scope.selectedCounter = 0;
 
+
+  $scope.change = function (item) {
+        if (item.selected) {
+            $scope.selectedCounter++
+        } else {
+            $scope.selectedCounter--
+        }
+    };
+
+  $scope.counterPositive=function(){
+    if($scope.selectedCounter == 0){
+      return false;
+    }
+    else{
+      return true;
+    }
+  };
+   $scope.counterMoreOne=function(){
+    if($scope.selectedCounter == 1){
+      return true;
+    }
+    else{
+      return false;
+    }
+  };
+ $scope.orderProp = 'name'
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
-  $scope.account = function(){
-    $state.go('tab.account');
-  };
-})
 
+for (var i = $scope.chats.length - 1; i >= 0; i--) {
+  $scope.chats[i].friendClicked=false;
+};
+
+  $scope.clickFriend=function($index){
+    if ($scope.chats[$index].friendClicked){
+      $scope.chats[$index].friendClicked=false;
+    }
+
+    else{
+     $scope.chats[$index].friendClicked=true;
+    }
+  }
+
+  $ionicModal.fromTemplateUrl('templates/newfriend.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal
+  })  
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  }
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+$ionicModal.fromTemplateUrl('templates/allfriend.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal1 = modal
+  })  
+
+  $scope.openModal1 = function() {
+    $scope.modal1.show();
+  }
+
+  $scope.closeModal1 = function() {
+    $scope.modal1.hide();
+  };
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  
+})
+.controller('NewFriendCtrl', function($scope, Chats, $state) {
+  })
+.controller('NewFriendCtrl', function($scope, Chats, $state) {
+  })
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats, ListLibrary, SongLibrary) {
  
   $scope.chat = Chats.get($stateParams.chatId);
@@ -72,9 +200,11 @@ $scope.account = function(){
   }; */
 
 })
-.controller('ListDetailCtrl', function($scope, $stateParams, ListLibrary, SongLibrary) {
+
+.controller('ListDetailCtrl', function($scope, $stateParams, ListLibrary, SongLibrary, $ionicHistory, $window, $localStorage) {
   $scope.chansons=SongLibrary.all();
   $stateParams.list = ListLibrary.get($stateParams.listId);
+  $scope.myFavorites=$localStorage.getObject('userFavoriteArray');
 
   $scope.imageClass=['','','','','','','','','','','','','','']; // va servir à l'affichage de l'image blurred
 
@@ -88,7 +218,35 @@ $scope.account = function(){
     $scope.selectedIndex = $index;
   };
 
-  
+  $scope.containSong = function(arrayOfSongs, song){
+    for (var i = arrayOfSongs.length - 1; i >= 0; i--) {
+      if(arrayOfSongs[i].id == song.id){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  $scope.addSongToFavorites = function(song){
+    $scope.myFavorites = $localStorage.getObject('userFavoriteArray');
+    if (!$scope.containSong($scope.myFavorites, song)){
+      $localStorage.addElement('userFavoriteArray',song);
+      $scope.myFavorites=$localStorage.getObject('userFavoriteArray');
+    }
+  };
+
+  $scope.removeSongFromFavorites = function(song){
+    $localStorage.removeElement('userFavoriteArray', song);
+    $scope.myFavorites=$localStorage.getObject('userFavoriteArray');
+  };
+
+  $scope.goBack = function(){
+    console.log('ici');
+
+    $scope.media.pause();
+    $ionicHistory.goBack();
+  };
+
 
   $scope.refreshBlurring= function(index){
     if ($scope.imageClass[index]=='my-class'){
@@ -104,50 +262,24 @@ $scope.account = function(){
   };
 
 
-  $scope.playSong = function(song) {
-    if ($scope.media.src===song.preview_url && $scope.isPlaying){
-      $scope.media.pause();
-      $scope.isPlaying = false;
-    }
 
-    else{
-      $scope.media.src = song.preview_url;
-      $scope.media.play();
-      $scope.isPlaying = true;
-    }
-
-  };
- 
-  /*$scope.playSong = function(song) {
-    $scope.defer = $q.defer();
-    $scope.media = new Audio(song.preview_url);
-
-    $scope.media.addEventListener("loadeddata", function(){
-       $scope.defer.resolve();
-     });
-
-  $scope.media.play();
-  return $scope.defer.promise;
-}   NE MARCHE PAS, DOIT-ON METTRE $SCOPE POUR TOUTES LES VAR DEFINIES ICI ?*/
 
   $scope.isPlaying=false;
   $scope.media = document.createElement('audio');
-  
-  $scope.playSong = function(song) {
-    if ($scope.media.src===song.preview_url && $scope.isPlaying){
-      $scope.media.pause();
-      $scope.isPlaying = false;
-      return null;
-    }
+    
+    $scope.playSong = function(song) {
+      if ($scope.media.src===song.preview_url && $scope.isPlaying){
+        $scope.media.pause();
+        $scope.isPlaying = false;
+        return null;
+      }
 
-    else{
-      $scope.media.src = song.preview_url;
-      $scope.media.play();
-      $scope.isPlaying = true;
-
-    }
-
-  };
+      else{
+        $scope.media.src = song.preview_url;
+        $scope.media.play();
+        $scope.isPlaying = true;
+      }
+    };
 
 
   $scope.stopSong = function() {
@@ -162,6 +294,8 @@ $scope.account = function(){
 
 
 })
+
+
 
 
 
@@ -191,11 +325,89 @@ $scope.account = function(){
       
     };
 })
-.controller('FavoritesCtrl', function($scope, $state) {
+.controller('FavoritesCtrl', function($scope, User, Chats, Profil, $state, SongLibrary,$window,$ionicModal, $localStorage) {
+
+  $scope.myFavorites = $localStorage.getObject('userFavoriteArray');
+  
+  $scope.refreshFavorites = function(){
+     $scope.myFavorites = $localStorage.getObject('userFavoriteArray'); 
+   };
+ 
+  
   $scope.settings = {
     enableFriends: true
   };
-  $scope.account = function(){
-    $state.go('tab.account');
+  $scope.settings = {
+    enableFriends: true
   };
+  $scope.profils = Profil.all();
+  $scope.showEdit = false;
+  $scope.editNameClick = function(){
+    $scope.showEdit = true;
+  };
+  $scope.logout=function(){
+    User.destroySession();
+     $scope.modal.hide();
+    $state.go('connexion');
+  }
+  $scope.addNewName=function(myName){
+      
+        $scope.profils.name=myName;
+        $scope.showEdit = false;
+      
+    };
+  $scope.showEditFav = false;
+  $scope.editFavClick = function(){
+    $scope.showEditFav = true;
+  };
+  $scope.addNewFav=function(myFav){
+      
+        $scope.profils.FavoritedSong=myFav;
+        $scope.showEditFav = false;
+      
+    };
+$ionicModal.fromTemplateUrl('templates/tab-account.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal
+  })  
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+  }
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  
+  $scope.removeSong = function(song, index) {
+    $scope.myFavorites.splice(index, 1);
+  };
+
+  $scope.openSong=function(song){
+    $window.open(song.open_url,"_system");
+  };
+  $scope.isPlaying=false;
+  $scope.media = document.createElement('audio');
+  $scope.playSong = function(song) {
+    if ($scope.media.src===song.preview_url && $scope.isPlaying){
+      $scope.media.pause();
+      $scope.isPlaying = false;
+      return null;
+    }
+
+    else{
+      $scope.media.src = song.preview_url;
+      $scope.media.play();
+      $scope.isPlaying = true;
+
+        }
+
+  };
 });
